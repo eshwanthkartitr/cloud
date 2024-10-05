03d85d5f24ced8d5fecfcf67a0b7156339957fa5
Eshwanth Karti T R <eshwanthkartitr@gmail.com>
2024-10-05 09:09:49 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+  res.json({ response: "Hello World" });
 });
+
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+  res.json({ response: "Great!, It works!" });
 });
+
 app.listen(process.env.PORT || 3000);
 module.exports = app;
